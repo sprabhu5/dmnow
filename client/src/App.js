eb891672d6bfe67b0530b61517d6da3ed20bff24
sprabhu5 <58062934+sprabhu5@users.noreply.github.com>
2021-05-11 00:41:36 -0400

    Updated_Send_Message

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import{ BrowserRouter as Router, Route} from 'react-router-dom';
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 
 
